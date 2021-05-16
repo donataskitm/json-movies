309c3c971980f38f053d5dfda4320f18ff5002fd
donataskitm <79906134+donataskitm@users.noreply.github.com>
2021-05-16 10:39:19 +0300

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Details = (movies) =>{
+
+return (
+
+    <div className="App">
+    <div key={movies.id}>
+      <p> {movies.Title}</p>
+      <img src={movies.Poster} alt=""/>
+      <p> Year: {movies.Year}</p>
+      <p> Released: {movies.Released}</p>
+      <p> Runtime: {movies.Runtime}</p>
+      <p> Genre: {movies.Genre}</p>
+      <p> Director: {movies.Director}</p>
+      <p> Writer: {movies.Writer}</p>
+      <p> Actors: {movies.Actors}</p>
+      <p> Type: {movies.Type}</p>
+      <p> Language: {movies.Language}</p>
+      <p> Imdb rating: {movies.imdbRating}</p>
+      <p> Imdb votes: {movies.imdbVotes}</p>
+      <p> Short description: {movies.Plot}</p>
+      <button onClick = {() =>  movies.showComponent("false")}>Back</button>
+     
+    </div>
+  </div>
+ 
+);
+}
+ //
+export default Details;
