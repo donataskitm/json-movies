446e4ef516f7e5b63f33028e33f6b110a801fecc
donataskitm <79906134+donataskitm@users.noreply.github.com>
2021-05-17 07:34:58 +0300

    Add files via upload

---
+++ Movie.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Movie = (movies) =>{
+
+return (
+ 
+  <div className="App"> <div key={movies.id}> 
+      <p> {movies.Title}</p>
+      <img src={movies.Poster} alt=""/>
+      <p> Movie type: {movies.Type}</p>
+      <p> Year: {movies.Year}</p>
+      <button onClick={ ()=>{movies.startSearch1(movies.imdbID); movies.startShow(false)}}>More info</button> 
+    </div>
+    <hr></hr>
+  </div>
+);
+}
+
+
+export default Movie;
